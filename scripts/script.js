df8002c948c294dc4eb6bec24f4e363d9b832d3d
Vladimir Scacodub <vladimir.scacodub@gmail.com>
2024-09-10 12:49:59 +0300

    our services menu added

--- scripts/script.js
+++ scripts/script.js
@@ -27,4 +27,32 @@ document.addEventListener('DOMContentLoaded', () => {
             control.className = 'controls'
         })
     });
+
+    const services = document.querySelectorAll(".service");
+
+    const serviceLogoClasses = ["branding", "design", "development"];
+    const serviceLogoFileNames = ["./img/branding.png", "./img/design.png", "./img/development.png"];    
+
+    for (let i = 0; i < services.length; i++) {
+        const readmoreLink = services[i].getElementsByTagName("a")[0];
+        const logo = services[i].getElementsByClassName(serviceLogoClasses[i]).item(0);
+        
+        services[i].addEventListener("mouseover", () => {
+            services[i].style.borderBottom = "3px solid #70BF73"
+            readmoreLink.style.opacity = 1;
+            logo.style.backgroundImage = "url('" + serviceLogoFileNames[i] + "')";
+        });
+
+        services[i].addEventListener("mouseout", () => {
+            services[i].style.borderBottom = "";
+            readmoreLink.style.opacity = 0.5;
+            logo.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6))"+
+                                        ",url(\"" + serviceLogoFileNames[i] + "\")";
+        });
+
+    }
+
+    const designLogo = document.querySelectorAll(".service .design");
+    const developmentLogo = document.querySelectorAll(".service .development");
+    const brandingLogo = document.querySelectorAll(".service .branding");
 });
